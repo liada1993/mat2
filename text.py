213ff5a341acd998a9736db561fa977200d3cd33
liada1993 <80317322+liada1993@users.noreply.github.com>
2021-03-29 14:13:38 +0300

    Add files via upload

---
+++ text.py
@@ -0,0 +1,14 @@
+def revword(word:str):
+    word=word.lower()
+    return(word[::-1])
+
+def countword() ->int:
+    text=open('text.txt','r')
+    text=text.read().split()
+    count=0
+    word=text[0]
+    for chek in text:
+            test=revword(chek)
+            if test==word:
+                count+=1
+    return(count+1)
